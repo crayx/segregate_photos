4937ed17b0d9283f7a2cb389908c0adabb71828a
meghnaY <meghnayadava2909@gmail.com>
2020-05-27 19:26:05 +0530

    Reset

--- script.sh
+++
@@ -1,8 +0,0 @@
-for file in segregate_photos/*.jpg
-do
-   	filename="$(basename "${file}")"
-   	year="$(echo "${filename}" | cut -b 1-4)"
-   	month="$(echo "${filename}" | cut -b 6)"
-   	mkdir -p segregate_photos/photos/$year/$month/
-   	mv $file segregate_photos/photos/$year/$month/.
-done
